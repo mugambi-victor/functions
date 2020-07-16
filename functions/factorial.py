63ae5a8d7fd4b7149f73987259f55be78b907e9d
unknown <victormmugambi@gmail.com>
2020-07-16 09:59:27 +0300

    python functions'

---
+++ functions/factorial.py
@@ -0,0 +1,9 @@
+#Write a Python function to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument.
+def factorial(n):
+	if n==0:
+		return 1
+	else:
+		return n * factorial(n-1)
+
+n=int(input("enter the number you want to calculate factorial"))
+print(factorial(n))
---
+++ functions/func.py
@@ -0,0 +1,9 @@
+#Write a Python function to multiply all the numbers in a list
+def fun():
+	a=[1,2,3,4,5]
+	s=1
+	for i in a:
+		s*=i
+	print(s)
+
+fun()
